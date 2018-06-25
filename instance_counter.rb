852bf5768388ed37641601d97478d80196bcdd12
bondar5471 <bondar54711745@gmail.com>
2018-06-25 11:49:58 +0300

    modules

--- instance_counter.rb
+++ instance_counter.rb
@@ -1,19 +1,17 @@
 module InstanceCounter
   def self.included(base)
     base.extend ClassMethod
-    base.send :include, InstanceMethod
+    base.send :include, InstanseMethod
   end
 
-module ClassMethod
-
-  attr_accessor :instances
-
-end
+  module ClassMethod
+    attr_accessor :instances
+  end
 
   protected
   module InstanseMethod
     protected
     self.class.instances ||= 0
-    self.class.instances +=1
+    self.class.instances += 1
   end
 end
