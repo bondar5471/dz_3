2f6d61607cea6e4eda4c54e9c7208644d0f974e7
bondar5471 <bondar54711745@gmail.com>
2018-06-26 10:35:22 +0300

    valid

--- validation.rb
+++ validation.rb
@@ -1,7 +1,9 @@
 module Validation
   def validate!
     raise "Номер не должен быть пустым" if number.nil?
-    raise "Номер не должен быть меньше 4 симоволов" if number.length < 4
+    raise "Номер не должен быть меньше 6 симоволов" if number.length <= 6
+    raise "Формат номера не подходит" if numder != /^(\d|[a-z]){3}-?((\d{2})|([a-z]{2}))$/i
+    raise "Имя станции не можеть быть меньше 3 символов" if name.length < 3
     true
   end
 end
